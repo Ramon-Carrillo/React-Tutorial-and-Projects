2f5c93003730df4b65e57ebc52c1e48826cbb3f5
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-07-13 23:59:12 -0500

    finished that format of prices

--- comfy-sloth/src/context/products_context.js
+++ comfy-sloth/src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -33,7 +36,7 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
 
-  const fetchproducts = async (url) => {
+  const fetchProducts = async (url) => {
     dispatch({ type: GET_PRODUCTS_BEGIN })
     try {
       const response = await axios.get(url)
@@ -44,8 +47,19 @@ export const ProductsProvider = ({ children }) => {
     }
   }
 
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const product = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: product })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+    }
+  }
+
   useEffect(() => {
-    fetchproducts(url)
+    fetchProducts(url)
   }, [])
   return (
     <ProductsContext.Provider
@@ -53,6 +67,7 @@ export const ProductsProvider = ({ children }) => {
         ...state,
         openSidebar,
         closeSidebar,
+        fetchSingleProduct,
       }}
     >
       {children}
